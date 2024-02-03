6925c91a823b6e905c2a47ecdf7ef1c0f79f3500
Byron Kruger <bkruger@jhb.dvt.co.za>
2024-02-03 12:11:50 +0200

    Added CORS headers

--- Program.cs
+++ Program.cs
@@ -10,13 +10,17 @@ builder.Services.AddDbContext<DataContext>((opts) =>
     opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-app.UseAuthorization();
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
 
 app.MapControllers();
 
